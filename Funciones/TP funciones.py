5601f67635c5465e5730664154f886c53eda663c
Pato-001 <patrio.1306@gmail.com>
2025-10-23 14:57:48 -0300

    Update TP funciones.py

--- Funciones/TP funciones.py
+++ Funciones/TP funciones.py
@@ -36,7 +36,7 @@ print("Horas:", segundos_a_horas(segundos))
 #Ejercicio 6
 def tabla_multiplicar(numero):
     for i in range(1, 11):
-    print(f"{numero} x {i} = {numero * i}")
+        print(f"{numero} x {i} = {numero * i}")
 numero = int(input("Ingrese un número para ver su tabla de multiplicar: "))
 tabla_multiplicar(numero)
 
